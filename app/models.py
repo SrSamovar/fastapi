32c9329cdb52c4db4167637b1529542ec0fc6f15
Vadim Levchenko <levchenkovadim2001@mail.ru>
2025-01-09 21:58:22 +0300

    change models

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 import datetime
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+import uuid
+from custom_types import Role
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy import Integer, String, Boolean, DateTime, func
+from sqlalchemy import Integer, String, Boolean, DateTime, func, UUID, ForeignKey, CheckConstraint
 import os
-
 
 POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'LSamovar69')
@@ -26,6 +27,36 @@ class Base(DeclarativeBase, AsyncAttrs):
         return {'id': self.id}
 
 
+class User(Base):
+    __tablename__ = 'user'
+    __tableargs__ = (CheckConstraint('role in ("user", "admin")'))
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    tokens: Mapped[list['Token']] = relationship("Token", lazy='joined', back_populates='user')
+    advertisements: Mapped[list['Advertisement']] = relationship('Advertisement',
+                                                                 lazy='joined',
+                                                                 back_populates='user')
+    role: Mapped[Role] = mapped_column(String, default='user')
+
+
+class Token(Base):
+    __tablename__ = 'token'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    token: Mapped[uuid.UUID] = mapped_column(UUID,
+                                             unique=True,
+                                             server_default=func.gen_random_uuid())
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped[User] = relationship(User, lazy='joined', back_populates='tokens')
+
+    @property
+    def dict_(self):
+        return {'token': self.token}
+
+
 class Advertisement(Base):
     __tablename__ = 'advertisement'
 
@@ -35,6 +66,7 @@ class Advertisement(Base):
     price: Mapped[int] = mapped_column(Integer, nullable=False)
     author: Mapped[str] = mapped_column(String, nullable=False)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    user: Mapped[User] = relationship(User, lazy='joined', back_populates='advertisements')
 
     @property
     def dict_(self):
@@ -48,8 +80,8 @@ class Advertisement(Base):
         }
 
 
-ORM_OBJ = Advertisement
-ORM_CLS = type[Advertisement]
+ORM_OBJ = Advertisement | User | Token
+ORM_CLS = type[Advertisement] | type[User] | type[Token]
 
 
 async def init_orm():
